be403613b2e1eda802ce83444b653ce89b4de245
David Laganière <davidlag@gmail.com>
2018-07-25 09:11:53 -0400

    Various changes to implement the model. A lot more changes are still required.

--- main/admin.py
+++ main/admin.py
@@ -1,29 +1,63 @@
 from django.contrib import admin
+from django import forms
 
 from .models import Transaction, Store, Account, Category, SubCategory, Status, Tag
 
 
-class StoreInline(admin.TabularInline):
-    model = Store
-    #extra = 3
+class TransactionAdminForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
 
-
-class AccountInline(admin.TabularInline):
-    model = Account
+        fields = ("transaction_date",
+                "report_description",
+                "custom_description",
+                "expense",
+                "revenue",
+                "store",
+                "account",
+                "category",
+                "subcategory",
+                "status")
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('Transaction Date', {'fields': ['transaction_date'], 'classes': ['collapse']}),
-        (None,               {'fields': ['report_description']}),
-        (None,               {'fields': ['custom_description']}),
-        (None,               {'fields': ['expense']}),
-        (None,               {'fields': ['revenue']}),
-    ]
-    inlines = [StoreInline, AccountInline]
-    list_display = ('transaction_date', 'custom_description', 'expense', 'revenue')
-    #list_display = ('question_text', 'pub_date')
-    #list_filter = ['pub_date']
-    search_fields = ['transaction_date', 'report_description', 'custom_description', 'expense', 'revenue']
+    list_display = ("transaction_date_formatted",
+                    "custom_description",
+                    "expense",
+                    "revenue",
+                    "store",
+                    "account",
+                    "category",
+                    "subcategory",
+                    "status")
+
+    list_select_related = ("store",)
+
+    """
+    search_fields = ["transaction_date_formatted",
+                    "report_description",
+                    "custom_description",
+                    "expense",
+                    "revenue",
+                    "store",
+                    "account",
+                    "category",
+                    "subcategory",
+                    "status"]
+    """
+
+    form = TransactionAdminForm
+
+    def transaction_date_formatted(self, obj):
+        return obj.transaction_date.strftime("%Y-%m-%d")
+
+    transaction_date_formatted.admin_order_field = "transaction_date"
+    transaction_date_formatted.short_description = "Date"
 
 admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Store)
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Status)
+admin.site.register(Tag)
--- main/models.py
+++ main/models.py
@@ -4,14 +4,51 @@ from django.db import models
 from django.utils import timezone
 
 
+class Store(models.Model):
+    store_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.store_name
+
+
+class Account(models.Model):
+    account_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.account_name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.category_name
+
+
+class SubCategory(models.Model):
+    subcategory_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.subcategory_name
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.status_name
+
+
 class Transaction(models.Model):
     transaction_date = models.DateTimeField('transaction date')
-    report_description = models.CharField(max_length=200)
-    custom_description = models.CharField(max_length=200)
+    report_description = models.CharField(blank=False, null=False, max_length=200)
+    custom_description = models.CharField(blank=True, null=False, max_length=200)
     expense = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=2)
     revenue = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=2)
+    store = models.ForeignKey(Store, blank=True, null=True, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, blank=False, null=False, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, blank=False, null=False, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, blank=True, null=True, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, blank=False, null=False, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.custom_description
+
     #def was_published_recently(self):
     #    now = timezone.now()
     #    return now - datetime.timedelta(days=1) <= self.pub_date <= now
@@ -20,43 +57,8 @@ class Transaction(models.Model):
     #was_published_recently.short_description = 'Published recently?'
 
 
-class Store(models.Model):
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-    store_name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.store_name
-
-
-class Account(models.Model):
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-    account_name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.account_name
-
-
-class Category(models.Model):
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-    category_name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.category_name
-
-
-class SubCategory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    subcategory_name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.subcategory_name
-
-
-class Status(models.Model):
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-    status_name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.status_name
-
-
 class Tag(models.Model):
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    transaction = models.ForeignKey(Transaction, null=False, on_delete=models.CASCADE)
     tag_name = models.CharField(max_length=200)
     def __str__(self):
         return self.tag_name
