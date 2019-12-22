5507baac108f852d097458fa0b3234cece30eb2b
David Laganière <davidlag@gmail.com>
2019-12-21 19:14:38 -0500

    Linting fix.

--- sequoia/settings/production.py
+++ sequoia/settings/production.py
@@ -1,14 +1,13 @@
 # pylint: disable=wildcard-import,unused-wildcard-import
 """Production Settings"""
 from .base import *
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sequoia_api',
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST')
-    }
-}
-"""
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'sequoia_api',
+#        'USER': config('DB_USER'),
+#        'PASSWORD': config('DB_PASSWORD'),
+#        'HOST': config('DB_HOST')
+#    }
+#}
