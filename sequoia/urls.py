df1180873332b5ee0a8260f04a0386bd780b5cd5
David Laganière <davidlag@gmail.com>
2020-01-04 10:20:57 -0500

    Reworked root URL's.

--- sequoia/urls.py
+++ sequoia/urls.py
@@ -1,13 +1,11 @@
 """Application URL's"""
-from django.conf.urls import url
+#from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 # from django.contrib.auth import views as auth_views
 #from graphene_django.views import GraphQLView
 #from sequoia.schema import schema
 #from django.views.decorators.csrf import csrf_exempt
-
-from . import views
 
 urlpatterns = [
     # To test before removing the main app.
@@ -17,8 +15,6 @@ urlpatterns = [
     # path('accounts/password_reset/', auth_views.PasswordResetView.as_view(),
     # name='password_reset'),
     path('admin/', admin.site.urls),
-
-    #url(r'^api$', views.api_root),
     path('api/transactions/', include('transactions.urls')),
     # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
