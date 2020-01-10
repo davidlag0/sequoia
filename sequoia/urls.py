fae7cca10d22c5c5ab9d4fedde9fcb03682d5826
David Laganière <davidlag@gmail.com>
2020-01-10 08:12:07 -0500

    Added redoc support.

--- sequoia/urls.py
+++ sequoia/urls.py
@@ -1,20 +1,24 @@
 """Application URL's"""
-#from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-# from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 #from graphene_django.views import GraphQLView
 #from sequoia.schema import schema
 #from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    # To test before removing the main app.
-    # path('', include('main.urls')),
-    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    # path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('accounts/password_reset/', auth_views.PasswordResetView.as_view(),
-    # name='password_reset'),
     path('admin/', admin.site.urls),
+    path('openapi/',
+         get_schema_view(title="Sequoia API",
+                         description="API for Sequoia Back-End"
+                        ),
+         name='openapi-schema'),
+    path('doc/',
+         TemplateView.as_view(template_name='redoc/redoc.html',
+                              extra_context={'schema_url':'openapi-schema'}
+                             ),
+         name='redoc'),
     path('api/transactions/', include('transactions.urls')),
     # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
